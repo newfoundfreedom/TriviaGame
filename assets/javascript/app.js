fa30b17f36665d3ac3485bc2eced1de4ff08be76
joel <joel@YAVIN-IV.local>
2017-03-02 08:27:12 -0500

    added questionDipsplay & randomQuestionSelection functions to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,67 +14,249 @@ var bbTrivia = {
     qaBank: {
         1: {
             question: 'Walter White\'s transformation from nerdy high school chemistry teacher to criminal kingpin all started with what specific medical diagnosis?',
-            g1: 'Pancreatic cancer',
-            g2: 'Lupus',
-            g3: 'AIDS',
-            g4: 'Lung cancer',
-            answer: 'Lung cancer'
+            answers: ['Pancreatic cancer', 'Lupus', 'AIDS', 'Lung cancer'],
+            correct: 'Lung cancer'
         },
         2: {
             question: 'What nickname does Jesse go by before he joins up with Mr. White?',
-            g1: 'Meth Man',
-            g2: 'Cap\'n Cook',
-            g3: 'K1ngpin',
-            g4: 'Crazy 8',
-            answer: 'Cap\'n Cook'
+            answers: ['Meth Man', 'Cap\'n Cook', 'K1ngpin', 'Crazy 8'],
+            correct: 'Cap\'n Cook'
         },
         3: {
             question: 'What is the name of the car wash Walter works at to make ends meet?',
-            g1: '33rd Street Car Wash',
-            g2: 'Scrub\'n Suds',
-            g3: 'A1A Car Wash',
-            g4: 'AAA Car Sparkle',
-            answer: 'A1A Car Wash'
+            answers: ['33rd Street Car Wash', 'Scrub\'n Suds', 'A1A Car Wash', 'AAA Car Sparkle'],
+            correct: 'A1A Car Wash'
+        },
+        4: {
+            question: 'Saul suggests that Walter may want to use a "guy" he has that can make Walter disappear. What is the type of business that serves as a front for this disappearance service?',
+            answers: ['Vacuum shop', 'Computer repair store', 'Pest control company', 'Car wash'],
+            correct: 'Vacuum shop'
+        },
+        5: {
+            question: 'What substance is Lydia always asking for, for her tea, that eventually ends up being linked to her likely death in the final episode of the series?',
+            answers: ['Saccharin', 'Stevia', 'Splenda', 'Brown sugar'],
+            correct: 'Stevia'
+        },
+        6: {
+            question: 'Walter White In the final episode, who is the lone survivor of Walters revenge attack on Jack Welker and his men.',
+            answers: ['Walter White', 'Jesse Pinkman', 'Jack Welker', 'Todd Alquist'],
+            correct: 'Jesse Pinkman'
+        },
+        7: {
+            question: 'What is the name of Jesse’s younger brother?',
+            answers: ['John', 'Jake', 'James', 'Josh'],
+            correct: 'Jake'
+        },
+        8: {
+            question: 'What does Walt Jr claim that veggie bacon smells like in the first ever episode?',
+            answers: ['Cabbage', 'A trash can', 'Band-Aids', 'Medicine'],
+            correct: 'Band-Aids'
+        },
+        9: {
+            question: 'What was the name of the meth that Jesse initially made and distributed?',
+            answers: ['Cypress T', 'Crazy 8', 'Cherry 6', 'Chili P'],
+            correct: 'Chili P'
+        },
+        10: {
+            question: 'What does Lydia say the average purity of meth is in the Czech Republic?',
+            answers: ['40%', '50%', '60%', '70%'],
+            correct: '60%'
+        },
+        11: {
+            question: 'What is the name of the acid that is used to corrosively dispose of Walter’s victims?',
+            answers: ['Hydrochloric Acid', 'Hydrofluoric Acid', 'Hydrochloride', 'Sulfuric Acid'],
+            correct: 'Hydrofluoric Acid'
+        },
+        12: {
+            question: 'What was the name of the plant that Walt used to poison Brock?',
+            answers: ['Lily of the Valley', 'Wollemi Pine', 'Yellow Jessamine', 'Palma Christi'],
+            correct: 'Lily of the Valley'
+        },
+        13: {
+            question: 'What name did Walt Jr adopt during his teen rebellion phase?',
+            answers: ['Scotty', 'Flynn', 'Bane', 'Jack'],
+            correct: 'Flynn'
+        },
+        14: {
+            question: 'What is Walt’s main car in the series?',
+            answers: ['1986 Toyota Tercel', '2004 Pontiac Aztec', '2006 Chrysler PT Cruiser', '2002 Toyota RAV4'],
+            correct: '2004 Pontiac Aztec'
+        },
+        15: {
+            question: 'What does Walter White use to kill Krazy-8 (Maximinio Arciniega)?',
+            answers: ['Bike lock', 'Screwdriver', 'Bunsen Burner', 'Knife'],
+            correct: 'Bike lock'
+        },
+        16: {
+            question: 'What flavor Venezia Pizza does Walt end up hurling on to the garage roof?',
+            answers: ['Ham and Pineapple', 'Pepperoni', 'Tuna and Olive', 'Sausage & Onion'],
+            correct: 'Pepperoni'
+        },
+        17: {
+            question: 'What is Walter White’s middle name?',
+            answers: ['Hartwell', 'Archibald', 'Carlos', 'Whitman'],
+            correct: 'Hartwell'
+        },
+        18: {
+            question: 'What was the name of the video game that Jessie plays during “Problem Dog”?',
+            answers: ['Rage', 'Shooter', 'Hitman', 'Knockout'],
+            correct: 'Rage'
+        },
+        19: {
+            question: 'In season two, an exhibition of which artist did Jane take Jesse to see?',
+            answers: ['Andy Warhol', 'Georgia O’Keeffe', 'Jean-Michel Basquiat', 'Roy Lichtenstein'],
+            correct: 'Georgia O’Keeffe'
+        },
+        20: {
+            question: 'Jesse and Badger used to be in a band together – what was it called?',
+            answers: ['Spewdriver', 'Schlepladder', 'Twaughthammer', 'Generaldrill'],
+            correct: 'Twaughthammer'
+        },
+        21: {
+            question: 'The two cartoon chickens in the company logo for Los Pollos Hermanos are wearing:',
+            answers: ['Sunglasses', 'Stripy Scarves', 'Ponchos', 'Flip-Flops'],
+            correct: 'Ponchos'
+        },
+        22: {
+            question: 'What was the website created by Walt Jr to raise donation money for his father’s rising medical costs, which was later used by Saul Goodman to launder Walt’s drug money?',
+            answers: ['helpcuremydad.com', 'savewalterwhite.com', 'savemydad.com', 'curewalterwhite.com'],
+            correct: 'savewalterwhite.com'
+        },
+        23: {
+            question: 'What is Mike\'s granddaugher\'s name?',
+            answers: ['Kaylee', 'Jenny', 'Bailee', 'Maxine'],
+            correct: 'Kaylee'
+        },
+        24: {
+            question: 'What was the name of Gus’s original partner in the 80s?',
+            answers: ['James Martinez', 'Hector Salamanca', 'Max Arciniega', 'Hugo Siero'],
+            correct: 'Max Arciniega'
+        },
+        25: {
+            question: 'In season four, Mike gets shot in which part of his body?',
+            answers: ['His Forehead', 'His Chin', 'His Ear', 'His Nose'],
+            correct: 'His Ear'
+        },
+        26: {
+            question: 'In season four, Skyler and Walter begin attending meetings of which organization?',
+            answers: ['Alcoholics Anonymous', 'Gamblers Anonymous', 'Overeaters Anonymous', 'Narcotics Anonymous'],
+            correct: 'Gamblers Anonymous'
+        },
+        27: {
+            question: 'In season four, which character operates a forklift for Walter while Jesse is gone?',
+            answers: ['Gale', 'Tyrus', 'Keith', 'Corbin'],
+            correct: 'Tyrus'
+        },
+        28: {
+            question: 'Walter puts a GPS tracking bug on which character’s car?',
+            answers: ['Hank', 'Mike', 'Jesse', 'Gus'],
+            correct: 'Gus'
+        },
+        29: {
+            question: 'Which character is audited by the IRS?',
+            answers: ['Skyler', 'Saul', 'Hank', 'Ted'],
+            correct: 'Ted'
+        },
+        30: {
+            question: 'What is the name of the company founded by Walter’s former chemistry assistant, Gretchen?',
+            answers: ['Schwartz Pharmaceuticals', 'West Road Chemical', 'Schwartz Research, Inc.', 'Gray Matter'],
+            correct: 'Gray Matter'
+        },
+        31: {
+            question: 'In season 1, what does Jesse say “is sacred”?',
+            answers: ['Flipping a Coin', 'Promises', 'Pinky Swear', 'Handshake'],
+            correct: 'Flipping a Coin'
+        },
+        32: {
+            question: 'In season 1, Walter hallucinates a student saying what word instead of “midterm”?',
+            answers: ['Murder', 'Massacre', 'Meth', 'Mayhem'],
+            correct: 'Murder'
+        },
+        33: {
+            question: 'In season 1, Walter learns that Krazy-8’s father owns what kind of store?',
+            answers: ['Furniture', 'Electronics', 'Hardware', 'Grocery'],
+            correct: 'Furniture'
+        },
+        34: {
+            question: 'In season 1, what name does Hank choose for his meth-busting operation?',
+            answers: ['Icebreaker', 'Black Ice', 'Thin Ice', 'Ice Man'],
+            correct: 'Icebreaker'
+        },
+        35: {
+            question: 'In season four, what gift does Gus bring to Don Eladio?',
+            answers: ['Money', 'Tequila', 'A Car', 'A Ring'],
+            correct: 'Tequila'
         }
 
     },
 
-    gameQuestions: [],
+    gameQuestions: [],  // container to hold the randomly selected question numbers for current game
     playerGuesses: [],
     counter: 0,
     intervalID: null,
+
 
 //
 // TODO: Create Initialize function
 // - for loop to run 10 times to select 10 unique questions for qaBank for each quiz
 //
+
 // TODO ------------------------------------------------------------------
 //Create on-click function to Start quiz (start button)
 // - runs initialize function
 // - IntervalID = setInterval to run serveQuestion function every 15 seconds
 // TODO ------------------------------------------------------------------
 
-    qaDisplay: function (x) {
-        $('#qStage').text(this.qaBank[x].Question);  // Display selected Question to screen
-        var tempArray = ['g1', 'g2', 'g3', 'g4'];
-        for (num in tempArray) {
-            // TODO -------------------------------------------------------------------------
-            //Create randomize function here to switch questions up everytime
-            // var rando = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
-            // tempArray.splice(rando, 1);
-            // TODO -------------------------------------------------------------------------
-            // var ans = this.qaBank[x] + '.G' + num
-            // $('#aStage').html('<div>' + this.qaBank[x].ans + '</div>');
-        };
-    }
+    startGame: function(){
+        this.gameQuestions = [];
+        this.gameGuesses = [];
+        this.counter = 0;
+        this.intervalID = null;
+
+        this.tenRandoQuestions(this.qaBank);
+
+    },
 
 
-// TODO: Create serveQuestion function
+
+
+
+
+    tenRandoQuestions: function (array) {
+        var tempArray = Object.keys(bbTrivia.qaBank);
+        $.each([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function (index, value) {
+            var questionIndex = tempArray[Math.floor(Math.random() * tempArray.length)];
+            bbTrivia.gameQuestions.push(tempArray[questionIndex]);
+            tempArray.splice(questionIndex,1);
+        })
+    },
+//
+// TODO: Create qDisplay function
 // - iterates counter
 // - randomly chooses order in which to show answers
-// - presents question and answers (creation of divs - #question, #options)
 // - timeout for 2 seconds before running timer function to allow player to read question
-//
+
+
+    qDisplay: function (x) {                                                         //function that takes the question number as an argument then,
+        $('#qStage').text(this.qaBank[x].question);                                  // Displays selected question to screen
+
+        for (item in this.qaBank[x].answers) {                                       // Iterates through answers array for given question
+            var answer = this.qaBank[x].answers[item];
+            $('#aStage').append('<div><label class="btn btn-primary answer-options"><input type="radio" name="options" value=' + answer + '/>' + answer + '</label></div>');  // and displays the questions as radio buttons
+        }
+
+
+        // TODO -------------------------------------------------------------------------
+        //Create randomize function here to switch questions up everytime
+        // var rando = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+        // tempArray.splice(rando, 1);
+        // TODO -------------------------------------------------------------------------
+        // var ans = this.qaBank[x] + '.G' + num
+        // $('#aStage').html('<div>' + this.qaBank[x].ans + '</div>');
+    }
+    ,
+
+
 // TODO: questionTimer function
 // - if player guesses question before timer runs out
 // - stop timer
@@ -88,3 +270,5 @@ var bbTrivia = {
 // - re-display questions and answers marking the one they guessed and the correct one
 
 } // close bbTrivia object
+
+bbTrivia.startGame();
